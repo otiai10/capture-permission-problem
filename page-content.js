56ee16507131fb6a3066e195baaa9dce2ed5e53c
otiai10 <otiai10@gmail.com>
2014-04-18 13:05:33 +0900

    Fix problem: @see https://developer.chrome.com/extensions/activeTab#invoking-activeTab

--- page-content.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('keyup', function(keyboardEvent) {
-    var TRIGGER_KEY = 88;// "Shift + x" for example
-    if(keyboardEvent.shiftKey && keyboardEvent.keyCode === TRIGGER_KEY){
-        chrome.runtime.sendMessage(null, {});
-    }
-});
